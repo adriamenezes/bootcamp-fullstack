fd485ef10069b11b077d47d5be5a426fc5ec5dfa
adria-menezes <adriah.menezes@gmail.com>
2020-05-18 14:02:21 -0300

    Add: aula05-javascript-introducao

---
+++ modulo01-fundamentos/aula05-javascript-introducao/js/script.js
@@ -0,0 +1,10 @@
+console.log('Olá, mundo!');
+
+var title = document.querySelector('h1');
+title.textContent = 'Modificado por Adria Menezes';
+
+// Comentário de linha
+
+/*
+Comentário de bloco
+*/
